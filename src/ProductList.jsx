23ff8f13eefaf58227d380e552171e75ef75de4a
Syukri <msyukri_mf@yahoo.com>
2025-02-27 14:07:13 -0500

    Adding useSelector and useDispatch

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,11 +1,14 @@
 import React, { useState,useEffect } from 'react';
 import { addItem } from './CartSlice';
+import { useSelector, useDispatch } from 'react-redux';
 import './ProductList.css'
 import CartItem from './CartItem';
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
+    const cart = useSelector((state) => state.cart);
+    const dispatch = useDispatch();
 
     const plantsArray = [
         {
